8b8692e3b28d158182d152c901e8abb7786f7664
Martin Fillon <martin.fillon@epitech.eu>
2022-12-08 10:42:26 +0100

    [FIX] query for architect

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.get("/architect", (req, res) => {
 
 app.get("/calculus", (req, res) => {
     console.log(req);
-    let coos = coords.set_coords(req.body.x, req.body.y);
-    let translation_matrix = translation.set_translation(req.body.translation_x, req.body.translation_y);
-    let scaling_matrix = scaling.set_scaling(req.body.scaling_x, req.body.scaling_y);
-    let rotation_matrix = rotation.set_rotation(req.body.roation);
-    let reflection_matrix = reflection.set_reflection(req.body.reflection);
+    let coos = coords.set_coords(req.query.x, req.query.y);
+    let translation_matrix = translation.set_translation(req.query.translation_x, req.query.translation_y);
+    let scaling_matrix = scaling.set_scaling(req.query.scaling_x, req.query.scaling_y);
+    let rotation_matrix = rotation.set_rotation(req.query.roation);
+    let reflection_matrix = reflection.set_reflection(req.query.reflection);
     let final_matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
     final_matrix = matrix.multiplication_third(translation_matrix, final_matrix);
     final_matrix = matrix.multiplication_third(scaling_matrix, final_matrix);
@@ -35,10 +35,10 @@ app.get("/calculus", (req, res) => {
     let new_coos = matrix.set_new_position(final_matrix, coos);
     new_coos[0] = new_coos[0].toFixed(2);
     new_coos[1] = new_coos[1].toFixed(2);
-    res.write(`Translation along vector (${req.body.translation_y}, ${req.body.translation_x})\n`);
-    res.write(`Scaling by factors ${req.body.scaling_x} and ${req.body.scaling_y}\n`);
-    res.write(`Rotation by a ${req.body.roation} degree angle\n`);
-    res.write(`Reflection over an axis with an inclination angle of ${req.body.reflection} degrees\n`)
+    res.write(`Translation along vector (${req.query.translation_y}, ${req.query.translation_x})\n`);
+    res.write(`Scaling by factors ${req.query.scaling_x} and ${req.query.scaling_y}\n`);
+    res.write(`Rotation by a ${req.query.roation} degree angle\n`);
+    res.write(`Reflection over an axis with an inclination angle of ${req.query.reflection} degrees\n`)
     res.write(`${final_matrix[0][0].toFixed(2)} ${final_matrix[0][1].toFixed(2)} ${final_matrix[0][2].toFixed(2)}`);
     res.write('\n');
     res.write(`${final_matrix[1][0].toFixed(2)} ${final_matrix[1][1].toFixed(2)} ${final_matrix[1][2].toFixed(2)}`);
