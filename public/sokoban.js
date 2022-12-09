f2d47b12dd9f377848dc0b711deaede082ee28bb
Martin Fillon <martin.fillon@epitech.eu>
2022-12-09 10:46:19 +0100

    [DEBUG] map modification

--- public/sokoban.js
+++ public/sokoban.js
@@ -1,11 +1,11 @@
 import * as fs from "fs";
+let fileData = fs.readFileSync("tmp/map.txt");
+displayMap(fileData);
 
 function displayMap(map) {
     document.querySelector('h1').innerHTML = map;
 }
 
 window.addEventListener('keydown', function (e) {
-    let fileData = fs.readFileSync("tmp/map.txt");
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-    displayMap(fileData);
 }, false);
