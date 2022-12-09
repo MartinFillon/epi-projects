cf8780223a8a957b405585c6c8c399641fe4a733
Martin Fillon <martin.fillon@epitech.eu>
2022-12-09 09:51:53 +0100

    [DEBUG] map modification and map showing

--- public/sokoban.js
+++ public/sokoban.js
@@ -2,24 +2,20 @@ window.addEventListener('keydown', function (e) {
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
 }, false);
 
-function readSingleFile(e) {
+function readSingleFile(e, map) {
     let file = e.target.files[0];
     let reader = new FileReader();
     reader.onload = function (e) {
-        displayMap(e.target.result);
+        map = e.target.result;
     }
     reader.readAsText(file);
 }
 
-
-
 function displayMap (content) {
     document.querySelector('h1').innerHTML = content
 }
-let map = undefined;
-document.getElementById('file-input').addEventListener('change',  (e, map) => {
-    readSingleFile(e);
-    map = "This is map"
-    document.querySelector('h2').innerHTML = map;
+document.getElementById('file-input').addEventListener('change',  (e) => {
+    let map = "[Map]";
+    readSingleFile(e, map);
+    displayMap(map);
 }, false);
-console.log(map);
