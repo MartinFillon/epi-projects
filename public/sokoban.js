b6e477c8afac8c24e4622c0cb316acd24cc1266d
Martin Fillon <martin.fillon@epitech.eu>
2022-12-09 18:09:35 +0100

    [FIX] player movement

--- public/sokoban.js
+++ public/sokoban.js
@@ -45,7 +45,7 @@ function check_key(key) {
             let map_2d = get_2d_map(map);
             let playerPosition = getPlayerPosition(map_2d);
             displayPlayerPosition(playerPosition);
-            display2dMap(move_player(map_2d, key, playerPosition));
+            move_player(map_2d, key, playerPosition);
         }
     });
 }
@@ -95,21 +95,25 @@ function getPlayerPosition(map) {
 
 function move_player(map_2d, key, playerPosition) {
     let map = map_2d;
-    if (key === 'ArrowUp' && map[playerPosition.x - 1][playerPosition.y] === ' ') {
+    if (key === "ArrowUp" && map[playerPosition.x - 1][playerPosition.y] === ' ') {
         map[playerPosition.x][playerPosition.y] = ' ';
         map[playerPosition.x--][playerPosition.y] = 'P';
+        display2dMap(map);
     }
-    if (key === 'ArrowDown' && map[playerPosition.x + 1][playerPosition.y] === ' ') {
+    if (key === "ArrowDown" && map[playerPosition.x + 1][playerPosition.y] === ' ') {
         map[playerPosition.x][playerPosition.y] = ' ';
         map[playerPosition.x++][playerPosition.y] = 'P';
+        display2dMap(map);
     }
-    if (key === 'ArrowLeft' && map[playerPosition.x][playerPosition.y - 1] === ' ') {
+    if (key === "ArrowLeft" && map[playerPosition.x][playerPosition.y - 1] === ' ') {
         map[playerPosition.x][playerPosition.y] = ' ';
         map[playerPosition.x][playerPosition.y--] = 'P';
+        display2dMap(map);
     }
-    if (key === 'ArrowRight' && map[playerPosition.x][playerPosition.y + 1] === ' ') {
+    if (key === "ArrowRight" && map[playerPosition.x][playerPosition.y + 1] === ' ') {
         map[playerPosition.x][playerPosition.y] = ' ';
         map[playerPosition.x][playerPosition.y++] = 'P';
+        display2dMap(map);
     }
     return map;
 }
