f05fb14eaa84dc08e31f13fab7e8bb8fbaf584cd
Martin Fillon <martin.fillon@epitech.eu>
2022-12-09 15:06:22 +0100

    [FIX] map showing

--- public/sokoban.js
+++ public/sokoban.js
@@ -1,7 +1,3 @@
-import * as fs from "fs";
-let fileData = fs.readFileSync("tmp/map.txt");
-displayMap(fileData);
-
 function displayMap(map) {
     document.querySelector('h1').innerHTML = map;
 }
@@ -9,3 +5,18 @@ function displayMap(map) {
 window.addEventListener('keydown', function (e) {
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
 }, false);
+
+document.getElementById("file-input").addEventListener('change', readSingleFile, false);
+
+function readSingleFile(e) {
+    let file = e.target.files[0];
+    if (!file) {
+        return
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        let contents = e.target.result;
+        displayMap(contents);
+    };
+    reader.readAsText(file);
+}
