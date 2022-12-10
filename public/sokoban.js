86bfafddd0b1876b8d3ca113c1e614fd2401dbd7
Martin Fillon <martin.fillon@epitech.eu>
2022-12-10 12:35:06 +0100

    [DEBUG] Player movements on O

--- public/sokoban.js
+++ public/sokoban.js
@@ -124,7 +124,9 @@ function getObjectives(map) {
 function move_player(map_2d, key, playerPosition) {
     document.getElementById('objectives').innerHTML = "";
     for (let i = 0; i < OBJECTIVES_COUNT; i++) {
-        document.getElementById('objectives').innerHTML += JSON.stringify(OBJECTIVES[i]);
+        if (playerPosition.x === OBJECTIVES[i].ox && playerPosition.y === OBJECTIVES[i].oy) {
+            return move_player(map_2d, key, playerPosition, 'O');
+        }
     }
     return movePlayer(map_2d, key, playerPosition, ' ');
 }
